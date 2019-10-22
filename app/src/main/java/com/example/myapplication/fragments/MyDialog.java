6eefce78d6b05254ee7dd941d7249506fc73fdad
LAB B Trainer <omarbsoul@gmail.com>
2019-10-22 09:38:39 +0300

    init project

--- app/src/main/java/com/example/myapplication/fragments/MyDialog.java
+++ app/src/main/java/com/example/myapplication/fragments/MyDialog.java
@@ -3,15 +3,10 @@ package com.example.myapplication.fragments;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
 
@@ -20,9 +15,7 @@ public class MyDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext())
-                .setTitle("MyDialog")
-                .setMessage("This is fragment Dialog")
-                .setView(R.layout.custon_dialog);
+                .setView(R.layout.custom_dialog);
         return builder.create();
     }
 }
